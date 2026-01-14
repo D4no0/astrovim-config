e6e68da5825a7807a826266a6d5c9314a4eb1357
Daniel <d4no0007@gmail.com>
2026-01-14 15:03:54 +0200

    updated bindings

--- lua/custom/bindings.lua
+++ lua/custom/bindings.lua
@@ -1,6 +1,6 @@
 -- Switch buffers with tab
-vim.api.nvim_set_keymap("n", "<S-Tab>", ":bprev<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Tab>", ":bnext<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<S-Tab>", "[b", { noremap = false })
+vim.api.nvim_set_keymap("n", "<Tab>", "]b", { noremap = false })
 
 -- File viewer
-vim.api.nvim_set_keymap("n", "<Leader>e", "<Cmd>Neotree focus<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>e", "<Leader>o", { noremap = false })
